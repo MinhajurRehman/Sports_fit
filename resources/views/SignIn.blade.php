b2ca8ee19a11d5fda2b22a0b1c3edc9543bf0126
Minhaj-Portfolio <109561282+Minhaj-Portfolio@users.noreply.github.com>
2023-04-15 17:18:04 +0500

    project/2

--- resources/views/SignIn.blade.php
+++ resources/views/SignIn.blade.php
@@ -1,24 +1,41 @@
-<!--Trigger-->
-<a class="login-trigger" href="#" data-target="#login" data-toggle="modal">Login</a>
+<html>
 
-<div id="login" class="modal fade" role="dialog">
-    <div class="modal-dialog">
+<head>
+    <title>Sports Team</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Sports Team template project">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+    <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.theme.default.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/animate.css">
+</head>
 
-        <div class="modal-content">
-            <div class="modal-body">
-                <button data-dismiss="modal" class="close">&times;</button>
-                <h4>Login</h4>
-                <form>
-                    <input type="text" name="username" class="username form-control" placeholder="Username" />
-                    <input type="password" name="password" class="password form-control" placeholder="password" />
-                    <input class="btn login" type="submit" value="Login" />
-                </form>
+<body>
+    <!--Trigger-->
+    <a class="login-trigger" href="#" data-target="#login" data-toggle="modal">Login</a>
+
+    <div id="login" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button data-dismiss="modal" class="close">&times;</button>
+                    <h4>Login</h4>
+                    <form>
+                        <input type="text" name="username" class="username form-control" placeholder="Username" />
+                        <input type="password" name="password" class="password form-control" placeholder="password" />
+                        <input class="btn login" type="submit" value="Login" />
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</body>
 
-
+</html>
 
 {{-- CSS --}}
 <style>
